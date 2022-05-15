3f7b38b62c4a6fd013b7e38b9bc162e8889f5f59
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2022-05-16 05:35:49 +0700

    .

--- function/mongodb.js
+++ function/mongodb.js
@@ -1,6 +1,6 @@
 const { MongoClient } = require('mongodb');
-const url = 'mongodb://127.0.0.1:11211';
-// const url = 'mongodb://127.0.0.1:27017';
+// const url = 'mongodb://127.0.0.1:11211';
+const url = 'mongodb://127.0.0.1:27017';
 
 // const client = new MongoClient(url);
 // await client.connect();
