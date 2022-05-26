4268e9cd20625349107d85f6f25ea5aa7474bcc3
Thai Parkerizing <administrator@cpe-172-101-34-209.maine.res.rr.com>
2022-05-26 12:14:45 +0700

    .

--- flow/001/history.js
+++ flow/001/history.js
@@ -5,6 +5,30 @@ let mongodb = require('../../function/mongodb');
 var request = require('request');
 
 
+router.post('/gethistoryplant', async (req, res) => {
+
+    //-------------------------------------
+    console.log(req.body);
+    let input = req.body;
+    //-------------------------------------
+    let output = [];
+    let plant = input['plant']
+
+
+
+    let find = await mongodb.find(`${plant}_MASTER`, 'specification', {});
+    if (find.length > 0) {
+        // output = { "return": 'OK' }
+        for (i = 0; i < find.length; i++) {
+            output.push({ "MATNO": find[i]['MATNO'], "ProductName": find[i]['ProductName'], })
+        }
+
+    }
+
+
+
+    res.json(output);
+});
 
 
 router.post('/gethistory', async (req, res) => {
@@ -13,18 +37,43 @@ router.post('/gethistory', async (req, res) => {
     console.log(req.body);
     let input = req.body;
     //-------------------------------------
-    let output = [];
+    let output = {};
     let MATCP = `${input['MATCP']}`
     let plant = input['plant']
+    let outdata = [];
 
 
+    let find = await mongodb.find(`${plant}dbMAIN`, 'MAIN', { $and: [{ "MATNO": MATCP }] });
+    console.log(find);
+    if (find.length > 0) {
+        // output = find;
+   
+        output['checklist'] = find[0]['checklist'] ?? [];
+        for(i=0;i<find.length;i++){
 
-    let find = await mongodb.find(`${plant}dbMAIN`, 'MAIN', { $and: [ { "MATCP": MATCP } ] }); 
-    if(find.length > 0){
-        // output = { "return": 'OK' }
-        output = find;  
+            var databuff = {
+                "POID":find[i]['POID'],
+                "MATNO":find[i]['MATNO'],
+                "PO":find[i]['PO'],
+                "ProductName":find[i]['ProductName'],
+                "SumStatus":find[i]['SumStatus'],
+                "DEP":find[i]['DEP'],
+            }
+
+            for(j=0;j<output['checklist'].length;j++){
+                databuff[`P${j}_T1`] = find[i][`${output['checklist'][j]}`]['T1'] ?? ``;
+                databuff[`P${j}_T2`] = find[i][`${output['checklist'][j]}`]['T2'] ?? ``;
+                databuff[`P${j}_T3`] = find[i][`${output['checklist'][j]}`]['T3'] ?? ``;
+            }
+
+            
+
+            outdata.push(databuff);
+        }
     }
 
+    output['outdata'] = outdata;
+    // console.log(output);
 
     res.json(output);
 });
