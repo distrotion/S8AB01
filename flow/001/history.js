8f5e2ba3fc30e56f4dc1e15260e55790b942dde2
Thai Parkerizing <administrator@Thais-MacBook-Pro.local>
2023-11-21 01:51:16 +0700

    .

--- flow/001/history.js
+++ flow/001/history.js
@@ -193,18 +193,18 @@ router.post('/getweightlist', async (req, res) => {
                     };
                     for (let i = 0; i < datadb.length; i++) {
 
-                        if (GETPO[k] === `'${datadb[i]['NumOrder']}'` && StrChemicalList[s]===datadb[i]['StrChemical']) {
-                            newset['RecordTimeStart'] = datadb[i]['RecordTimeStart'];
-                            newset['PO'] = GETPO[k];
-                            // newset[StrChemicalList[s]+'_StrLotNum'] = datadb[i]['StrLotNum'];
-                            // newset[StrChemicalList[s]+'_StrBarcode'] = datadb[i]['StrBarcode'];
-                            // newset[StrChemicalList[s]+'_NumStep'] = datadb[i]['NumStep'];
-                            // newset[StrChemicalList[s]+'_NumSp'] = datadb[i]['NumSp'];
-                            // newset[StrChemicalList[s]+'_NumAct'] = datadb[i]['NumAct'];
-                            // newset[StrChemicalList[s]+'_NumTemp'] = datadb[i]['NumTemp'];
+                        // if (GETPO[k] === `'${datadb[i]['NumOrder']}'` && StrChemicalList[s]===datadb[i]['StrChemical']) {
+                        //     newset['RecordTimeStart'] = datadb[i]['RecordTimeStart'];
+                        //     newset['PO'] = GETPO[k];
+                        //     newset[StrChemicalList[s]+'_StrLotNum'] = datadb[i]['StrLotNum'];
+                        //     newset[StrChemicalList[s]+'_StrBarcode'] = datadb[i]['StrBarcode'];
+                        //     newset[StrChemicalList[s]+'_NumStep'] = datadb[i]['NumStep'];
+                        //     newset[StrChemicalList[s]+'_NumSp'] = datadb[i]['NumSp'];
+                        //     newset[StrChemicalList[s]+'_NumAct'] = datadb[i]['NumAct'];
+                        //     newset[StrChemicalList[s]+'_NumTemp'] = datadb[i]['NumTemp'];
                            
                        
-                        }
+                        // }
                     }
                 }
                 DATAOUTPUT.push(newset);
